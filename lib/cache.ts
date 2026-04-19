a6f4d7d67f59230b628fdf6765459d1c2c6bbbfd
Pedro Ambrosini <pedroalabousas@gmail.com>
2026-04-19 19:03:15 -0300

    feat: implements cache for servers api requests

--- lib/cache.ts
+++ lib/cache.ts
@@ -1,4 +1,5 @@
 import { Mod } from "@/types/mod";
+import { Server } from "@/types/server";
 import { unstable_cache } from "next/cache";
 
 export const getCache = unstable_cache(
@@ -27,6 +28,29 @@ export const getCache = unstable_cache(
         return mods as Mod[]
     },
     ['cache'],
+    {
+        revalidate: 300
+    }
+)
+
+export const getServersCache = unstable_cache(
+    async () => {
+        const v6res = await fetch("https://raw.githubusercontent.com/Anuken/Mindustry/refs/heads/master/servers_v6.json")
+        const v7res = await fetch("https://raw.githubusercontent.com/Anuken/Mindustry/refs/heads/master/servers_v7.json")
+
+        if (!v6res.ok || !v7res.ok) {
+            throw new Error(`Erro ao buscar data`)
+        }
+
+        let v6 = await v6res.json()
+        let v7 = await v7res.json()
+
+        v6 = v6.map((x: any) => ({ ...x, version: "v6" }))
+        v7 = v7.map((x: any) => ({ ...x, version: "v7" }))
+
+        return [...v6, ...v7] as Server[]
+    },
+    ['servers-cache'],
     {
         revalidate: 300
     }
