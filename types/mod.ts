9d501b2d0e3056b5d44bb69a3920223fa95d5e38
Pedro Ambrosini <pedroalabousas@gmail.com>
2026-04-19 18:23:56 -0300

    feat(types): add mod

---
+++ types/mod.ts
@@ -0,0 +1,12 @@
+export type Mod = {
+    id: string
+    name: string
+    repo: string
+    author: string
+    minGameVersion: string
+    lastUpdated: string
+    stars: number
+    hasScripts: boolean
+    hasJava: boolean
+    description: string
+}
