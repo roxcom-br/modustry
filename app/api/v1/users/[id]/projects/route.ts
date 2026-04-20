cb8dc9c25eb3897b55563b5e946c87fb3490dbb8
Pedro Ambrosini <pedroalabousas@gmail.com>
2026-04-19 23:16:03 -0300

    feat: implements user projects route

---
+++ app/api/v1/users/[id]/projects/route.ts
@@ -0,0 +1,30 @@
+import { getCache } from "@/lib/cache";
+import { parsePositiveInt } from "@/lib/utils";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(
+    request: NextRequest,
+    { params }: { params: Promise<{ id: string }> }
+) {
+    const { id } = await params
+    const { searchParams } = new URL(request.url)
+
+    const page = parsePositiveInt(searchParams.get('page'), 1);
+    const limit = parsePositiveInt(searchParams.get('limit'), 20);
+
+    let mods = await getCache()
+    mods = mods.filter(x => x.repo.split("/")[0] == id)
+
+    const start = (page - 1) * limit
+    const end = start + limit
+
+    return NextResponse.json({
+        data: mods.slice(start, end),
+        pagination: {
+            page,
+            limit, 
+            total: mods.length,
+            totalPages: Math.ceil(mods.length / limit)
+        }
+    })
+}
