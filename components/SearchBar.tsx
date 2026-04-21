e0ffc9c785f235be23d5d9e62a78f8a8edd87fae
Pedro Ambrosini <pedroalabousas@gmail.com>
2026-04-21 12:28:27 -0300

    feat: implement Search Bar component

---
+++ components/SearchBar.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import { useEffect, useState } from "react"
+
+type SearchBar = {
+    query: string,
+    onQueryChange: (query: string) => void
+}
+
+export default function SearchBar({ query, onQueryChange }: SearchBar) {
+    const [input, setInput] = useState(query)
+
+    useEffect(() => {
+        setInput(query)
+    }, [query])
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            if (input !== query) {
+                onQueryChange(input)
+            }
+        }, 300)
+
+        return () => clearTimeout(timeout)
+    }, [input, query, onQueryChange])
+
+    return (
+        <div className="flex mb-4">
+            <input 
+                className="w-full p-2 rounded-xl border-2 border-(--scheme-color-disabled) bg-(--scheme-color-secondary)"
+                type="text"
+                placeholder="Search..."
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+            />
+        </div>
+    )
+}
