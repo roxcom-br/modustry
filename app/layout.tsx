c93ef2e0c429a1a746223aa6998cd8bf3400afc7
Pedro Ambrosini <pedroalabousas@gmail.com>
2026-04-21 13:51:49 -0300

    feat: add css color scheme

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
     return (
         <html
             lang="en"
-            className="h-full antialiased"
+            className="h-full antialiased bg-(--background) text-(--color)"
         >
             <body className="min-h-full flex flex-col">{children}</body>
         </html>
