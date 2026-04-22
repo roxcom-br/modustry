bb09df1eb108804b0289466559770f1a4e10e92a
Pedro Ambrosini <pedroalabousas@gmail.com>
2026-04-22 09:21:24 -0300

    feat: implements Server Sidebar Info element

---
+++ components/servers/ServerSidebarInfo.tsx
@@ -0,0 +1,31 @@
+import Image from "@/components/utils/Image"
+import { Server } from "@/types/server"
+
+type ServerSidebarInfo = {
+    data: Server
+}
+
+export default function ServerSidebarInfo({ data }: ServerSidebarInfo) {
+    return (
+        <div className="flex flex-col bg-(--scheme-color-secondary) p-6 rounded-xl max-w-[320px]">
+            <Image
+                src={[
+                    '/icon.jpg',
+                ]}
+                className="rounded-xl"
+                alt="Mod Icon"
+                width={128}
+                height={128}
+            />
+            <div>
+                <h2 className="text-2xl font-semibold my-1">{data.name}</h2>
+            </div>
+            <hr className="my-4 border-zinc-200" />
+            <ul className="flex flex-col gap-1 list-disc ps-4">
+                {data.address.map((item) => (
+                    <li>{item}</li>
+                ))}
+            </ul>
+        </div>
+    )
+}
