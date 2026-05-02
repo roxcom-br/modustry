ee44211391b8b0824cd443205615c2315f9de892
Pedro Ambrosini <pedroalabousas@gmail.com>
2026-05-02 19:43:10 -0300

    feat: first version of mod changelog page

---
+++ app/mod/[id]/changelog/client.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import Markdown from "@/components/Markdown"
+import ModSidebarInfo from "@/components/mods/ModSidebarInfo"
+import ModsNavbar from "@/components/mods/ModsNavbar"
+import Navbar from "@/components/Navbar"
+import { Mod } from "@/types/mod"
+
+type ModChangelogClient = {
+    data: Mod,
+    changelog: {
+        name: string,
+        version: string,
+        body: string
+    }[]
+}
+
+export default function ModChangelogClient({ data, changelog }: ModChangelogClient) {
+    return (
+        <main>
+            <Navbar />
+            <section className="container mx-auto p-0 flex gap-4">
+                <ModSidebarInfo data={data} />
+                <div className="w-full">
+                    <ModsNavbar />
+                    <div className="flex flex-col gap-2">
+                        {changelog.map((value) => (
+                            <div key={value.version} className="bg-(--scheme-color-secondary) p-6 rounded-xl">
+                                <h2 className="text-3xl font-semibold mb-4">{value.name}</h2>
+                                <Markdown key={value.name} content={value.body} />
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </section>
+        </main>
+    )
+}
---
+++ app/mod/[id]/changelog/page.tsx
@@ -0,0 +1,46 @@
+import ModChangelogClient from "./client";
+import { remark } from "remark"
+import html from "remark-html"
+
+async function getData(id: string) {
+    const res = await fetch(`http://localhost:3001/api/v1/mods/${id}`, {
+        next: { revalidate: 300 }
+    })
+
+    if (!res.ok) throw new Error('Erro')
+
+    return await res.json()
+}
+
+async function getChangelogData(id: string) {
+    const res = await fetch(`http://localhost:3001/api/v1/mods/${id}/changelog`, {
+        next: { revalidate: 300 }
+    })
+
+    if (!res.ok) throw new Error('Erro')
+
+    return await res.json()
+}
+
+async function processMarkdown(value: string) {
+    return (await remark()
+        .use(html)
+        .process(value)).toString()
+}
+export default async function Mod({ params }: { params: Promise<{ id: string }> }) {
+    const { id } = await params
+
+    const data = await getData(id)
+    let changelog = await getChangelogData(id)
+
+    changelog = await Promise.all(changelog.map(async (x: any) => {
+        x.body = (await processMarkdown(x.body)).replaceAll("h2", "h3")
+        return x
+    }))
+
+    return (
+        <>
+            <ModChangelogClient data={data} changelog={changelog} />
+        </>
+    )
+}
