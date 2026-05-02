1efb6bd9485c54f1f2dbb8634fb855a69e9eb1a2
Pedro Ambrosini <pedroalabousas@gmail.com>
2026-05-02 19:41:12 -0300

    feat: mod changelog route

---
+++ app/api/v1/mods/[id]/changelog/route.ts
@@ -0,0 +1,27 @@
+import { getCache } from "@/lib/cache";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(
+    _req: NextRequest,
+    { params }: { params: Promise<{ id: string }> }
+) {
+    const { id } = await params
+
+    const mod = (await getCache()).find(x => x.id == id)
+
+    const res = await fetch(`https://api.github.com/repos/${mod!!.repo}/releases`)
+    
+    const json = await res.json()
+
+    let list = json.map((value: any) => (
+        {
+            name: value.name,
+            version: value.tag_name,
+            body: value.body,
+        }
+    ))
+
+    console.log(list)
+
+    return NextResponse.json(list)
+}
